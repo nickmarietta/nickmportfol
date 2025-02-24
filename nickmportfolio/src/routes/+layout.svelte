71045fb996ae3437a9b14f060d517543a20c1b3d
nickmarietta <nick.marietta@csu.fullerton.edu>
2025-02-24 14:59:57 -0800

    lefto off at 17:45. some changes need to be done if want to upgrade to new svelte

--- nickmportfolio/src/routes/+layout.svelte
+++ nickmportfolio/src/routes/+layout.svelte
@@ -1,6 +1,29 @@
 <script>
-    let { children } = $props();
     import "../app.css";
+    import Header from "../components/Header.svelte";
+    import Footer from "../components/Footer.svelte";
+
+    let y = 0;
+    let innerHeight = 0;
+    let innerWidth = 0;
+
+    function goTop() {
+        document.body.scrollIntoView();
+    }
 </script>
 
-{@render children()}
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+    <div>
+        <button>
+            <i class="fa-solid fa-arrow-up" />
+        </button>
+    </div>
+    
+    <Header />
+
+    <slot /> <!-- Old SvelteKit uses <slot> instead of {@render} -->
+
+    <Footer />
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
--- nickmportfolio/src/routes/+page.svelte
+++ nickmportfolio/src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<h2 class="text-green-300"> HARRO </h2>
+<script>
+    import Main from "../components/Main.svelte";
+
+</script>
+
+<Main />
