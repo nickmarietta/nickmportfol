2d4d12eaade93fda3d0a28d1abd9e112f5806436
nickmarietta <nick.marietta@csu.fullerton.edu>
2025-02-25 08:15:30 -0800

    fixed the header format

--- nickmportfolio/src/components/Main.svelte
+++ nickmportfolio/src/components/Main.svelte
@@ -6,11 +6,11 @@
     <section id="introPage" class=" grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                <span class="relative inline-block animate-bounce rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+                <span class="relative inline-block animate-bounce px-1 py-2 font-bold text-white hover:bg-blue-700">
                     Hi!
                 </span>
                 I'm 
-                <span class="lato text-violet-400">Nicklaus</span><br/> 
+                <span class="lato text-violet-400">Nicklaus</span>,<br/> 
                 an Aspiring
                 <span class="lato text-violet-400">Software Engineer</span>
                 
