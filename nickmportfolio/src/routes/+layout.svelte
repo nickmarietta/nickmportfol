b1ccf06b59f5e4005989157c5b7823e4626c3b8d
nickmarietta <nick.marietta@csu.fullerton.edu>
2025-02-24 20:35:02 -0800

    stage all changes. 29:46, fix the button issue with the hi

--- nickmportfolio/src/routes/+layout.svelte
+++ nickmportfolio/src/routes/+layout.svelte
@@ -13,13 +13,15 @@
 </script>
 
 <div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
-    <div>
-        <button>
-            <i class="fa-solid fa-arrow-up" />
+    <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (
+        y > 0 ? ' opacity-full pointer-events-auto': ' pointer-events-none opacity-0'
+        )}>
+        <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer">
+            <i class="fa-solid fa-arrow-up grid place-items-center" />
         </button>
     </div>
     
-    <Header />
+    <Header y={y}/>
 
     <slot /> <!-- Old SvelteKit uses <slot> instead of {@render} -->
 
