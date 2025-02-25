5c340e984ef47313bf956a1c1ac6a88de20ab735
nickmarietta <nick.marietta@csu.fullerton.edu>
2025-02-25 08:57:23 -0800

    33:28

--- nickmportfolio/src/components/Main.svelte
+++ nickmportfolio/src/components/Main.svelte
@@ -1,22 +1,39 @@
 <script>
-
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-    <section id="introPage" class=" grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
-        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+    <section
+        id="introPage"
+        class=" grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+    >
+        <div
+            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+        >
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                <span class="relative inline-block animate-bounce px-1 py-2 font-bold text-white hover:bg-blue-700">
+                <span
+                    class="relative inline-block animate-bounce px-1 py-2 font-bold text-white hover:bg-blue-700"
+                >
                     Hi!
                 </span>
-                I'm 
-                <span class="lato text-violet-400">Nicklaus</span>,<br/> 
+                I'm
+                <span class="lato text-violet-400">Nicklaus</span>,<br />
                 an Aspiring
                 <span class="lato text-violet-400">Software Engineer</span>
-                
             </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                Junior at <span class="text-violet-400">CSUF</span> studying
+                Computer Science. My
+                <span class="text-violet-400">favorite tech</span> includes JS (ReactJS,
+                SvelteKit), C++, and Python.
+            </p>
+            <a href="https://www.linkedin.com/in/nicklaus-marietta/" target="_blank" class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl lato relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
+                <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+                </div>
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+            </a>
         </div>
-
+        <div class="relative shadow-2xl grid place-itmes-center">
+            <img src="images/profile.png" alt="Profile Image">
+        </div>
     </section>
-
 </main>
